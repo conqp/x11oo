8952b6aa0297a0a1f5d3fafe0dd67001f1970284
Richard Neumann <r.neumann@homeinfo.de>
2022-11-24 16:42:51 +0100

    Remove comment

--- build.rs
+++ build.rs
@@ -1,7 +1,3 @@
-// x11-rs: Rust bindings for X11 libraries
-// The X11 libraries are available under the MIT license.
-// These bindings are public domain.
-
 extern crate pkg_config;
 
 use std::env;
