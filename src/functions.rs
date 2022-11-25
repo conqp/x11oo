6075c9949c0b4005bd7474fc8f3099f7e242e5c0
Richard Neumann <r.neumann@homeinfo.de>
2022-11-25 12:26:22 +0100

    Use new trait in functions.rs

--- src/functions.rs
+++ src/functions.rs
@@ -1,20 +1,18 @@
-use crate::util::discard_const_1;
+use crate::util::IsAlways;
 use std::borrow::BorrowMut;
 use std::ops::DerefMut;
 use x11::xlib::{XAddPixel, XAddToExtensionList, XExtData, XImage};
 
 pub fn add_pixel(image: &mut XImage, value: i64) {
-    discard_const_1(unsafe { XAddPixel(image, value) }, "XAddPixel")
+    unsafe { XAddPixel(image, value) }.is_always(1)
 }
 
 pub fn add_to_extension_list(structure: &mut &mut XExtData, ext_data: &mut XExtData) {
-    discard_const_1(
-        unsafe {
-            XAddToExtensionList(
-                (structure.deref_mut() as *mut XExtData).borrow_mut(),
-                ext_data,
-            )
-        },
-        "XAddToExtensionList",
-    )
+    unsafe {
+        XAddToExtensionList(
+            (structure.deref_mut() as *mut XExtData).borrow_mut(),
+            ext_data,
+        )
+    }
+    .is_always(1)
 }
