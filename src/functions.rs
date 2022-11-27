ae5b47ebb17c2f9fbbc8e01e4337e4618f9c2a18
Richard Neumann <mail@richard-neumann.de>
2022-11-27 23:33:33 +0100

    Use mutable reference to a raw pointer

--- src/functions.rs
+++ src/functions.rs
@@ -1,18 +1,11 @@
 use crate::util::PanicOnError;
 use std::borrow::BorrowMut;
-use std::ops::DerefMut;
 use x11::xlib::{XAddPixel, XAddToExtensionList, XExtData, XImage};
 
 pub fn add_pixel(image: &mut XImage, value: i64) {
     unsafe { XAddPixel(image, value) }.panic_if_zero()
 }
 
-pub fn add_to_extension_list(structure: &mut &mut XExtData, ext_data: &mut XExtData) {
-    unsafe {
-        XAddToExtensionList(
-            (structure.deref_mut() as *mut XExtData).borrow_mut(),
-            ext_data,
-        )
-    }
-    .panic_if_zero()
+pub fn add_to_extension_list(structure: &mut *mut XExtData, ext_data: &mut XExtData) {
+    unsafe { XAddToExtensionList((structure).borrow_mut(), ext_data) }.panic_if_zero()
 }
