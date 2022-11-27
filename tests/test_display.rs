1149452b6259cd06c2479e5ea930de227560228b
Richard Neumann <mail@richard-neumann.de>
2022-11-27 23:53:18 +0100

    Update tests

--- tests/test_display.rs
+++ tests/test_display.rs
@@ -3,10 +3,10 @@ use x11oo::Display;
 #[test]
 fn test_debug() {
     let display = Display::open(None::<String>);
-    assert!(display.is_some());
+    assert!(display.is_ok());
     println!("{:?}", display.unwrap());
 
     let display = Display::open(Some(":0"));
-    assert!(display.is_some());
+    assert!(display.is_ok());
     println!("{:?}", display.unwrap());
 }
