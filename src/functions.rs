14f7a87f5d2c77b0ca2d5d277d25e27a0c0e6ead
Richard Neumann <mail@richard-neumann.de>
2022-11-28 00:08:53 +0100

    Use NonNull in function params

--- src/functions.rs
+++ src/functions.rs
@@ -1,12 +1,11 @@
 use crate::util::PanicOnError;
-use std::borrow::BorrowMut;
 use std::ptr::NonNull;
 use x11::xlib::{XAddPixel, XAddToExtensionList, XExtData, XImage};
 
-pub fn add_pixel(image: &mut XImage, value: i64) {
-    unsafe { XAddPixel(image, value) }.panic_if_zero()
+pub fn add_pixel(image: NonNull<XImage>, value: i64) {
+    unsafe { XAddPixel(image.as_ptr(), value) }.panic_if_zero()
 }
 
-pub fn add_to_extension_list(structure: &mut NonNull<XExtData>, ext_data: &mut XExtData) {
-    unsafe { XAddToExtensionList(structure.as_ptr().borrow_mut(), ext_data) }.panic_if_zero()
+pub fn add_to_extension_list(structure: NonNull<*mut XExtData>, ext_data: NonNull<XExtData>) {
+    unsafe { XAddToExtensionList(structure.as_ptr(), ext_data.as_ptr()) }.panic_if_zero()
 }
