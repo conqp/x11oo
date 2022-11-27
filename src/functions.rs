e190c56d647aa02ac897662701bfb61ecfe8809f
Richard Neumann <mail@richard-neumann.de>
2022-11-27 23:57:51 +0100

    Update add_to_extension_list()

--- src/functions.rs
+++ src/functions.rs
@@ -1,11 +1,12 @@
 use crate::util::PanicOnError;
 use std::borrow::BorrowMut;
+use std::ptr::NonNull;
 use x11::xlib::{XAddPixel, XAddToExtensionList, XExtData, XImage};
 
 pub fn add_pixel(image: &mut XImage, value: i64) {
     unsafe { XAddPixel(image, value) }.panic_if_zero()
 }
 
-pub fn add_to_extension_list(structure: &mut *mut XExtData, ext_data: &mut XExtData) {
-    unsafe { XAddToExtensionList((structure).borrow_mut(), ext_data) }.panic_if_zero()
+pub fn add_to_extension_list(structure: &mut NonNull<XExtData>, ext_data: &mut XExtData) {
+    unsafe { XAddToExtensionList(structure.as_ptr().borrow_mut(), ext_data) }.panic_if_zero()
 }
