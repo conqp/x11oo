3952fd94ddf623b996f7c8427d02a3fff8401945
Richard Neumann <mail@richard-neumann.de>
2023-06-17 00:04:53 +0200

    Consume self

--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,10 @@
 pub(crate) trait PanicIfZero {
-    fn panic_if_zero(&self);
+    fn panic_if_zero(self);
 }
 
 impl PanicIfZero for i32 {
-    fn panic_if_zero(&self) {
-        if self == &0 {
+    fn panic_if_zero(self) {
+        if self == 0 {
             panic!("xlib function returned error")
         }
     }
